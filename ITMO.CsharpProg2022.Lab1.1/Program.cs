2e72b35e0f0f9820291559b6216b335186df29f3
pecherkin96 <maximal96@yandex.ru>
2022-05-15 20:32:13 +0300

    Добавьте файлы проекта.

---
+++ ITMO.CsharpProg2022.Lab1.1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CsharpProg2022.Lab1._1
+{
+    class Greeter
+    {
+        static void Main(string[] args)
+        {
+            string myName;
+
+            Console.WriteLine("Пожалуйста, введи своё имя.");
+            myName = Console.ReadLine();
+            Console.WriteLine("Привет, {0}", myName);
+        }
+    }
+}
---
+++ ITMO.CsharpProg2022.Lab1.4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CsharpProg2022.Lab1._4
+{
+    class DevideIt
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Пожалуйства, введите первое целое число.");
+                string temp = Console.ReadLine();
+                int i = Int32.Parse(temp);
+
+                Console.WriteLine("Пожалуйста, введите второе целое число.");
+                temp = Console.ReadLine();
+                int j = Int32.Parse(temp);
+
+                int k = i / j;
+                Console.WriteLine("Результат деления {2}. {0} поделив на {1}", i, j, k);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Выдано исключение: {0}", e);
+            }
+        }
+    }
+}
