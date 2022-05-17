6aa3a78d8034ff2ae025963a607a639329e54ced
pecherkin96 <maximal96@yandex.ru>
2022-05-17 11:16:03 +0300

    git push origin master

---
+++ ITMO.CsharpProg2022.Lab2.1/Enum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CsharpProg2022.Lab2._1
+{
+    public enum AccountType { Checking, Deposit }
+    class Enum
+    {
+        static void Main(string[] args)
+        {
+            AccountType goldAccount;
+            AccountType platinumAccount;
+            goldAccount = AccountType.Checking;
+            platinumAccount = AccountType.Deposit;
+            Console.WriteLine("The Customer Account Type is {0}", goldAccount);
+            Console.WriteLine("The Customer Account Type is {0}", platinumAccount);
+        }
+    }
+}
