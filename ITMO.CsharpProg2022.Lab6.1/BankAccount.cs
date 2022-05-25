7bf3e439d57e39487eac83bf7780efa4d1e677e5
pecherkin96 <maximal96@yandex.ru>
2022-05-25 18:22:49 +0300

    With Lab 6

---
+++ ITMO.CsharpProg2022.Lab6.1/BankAccount.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CsharpProg2022.Lab6._1
+{
+    class BankAccount
+    {
+        public void Populate(long number, decimal balance)
+        {
+            accNo = number;
+            accBal = balance;
+            accType = AccountType.Checking;
+        }
+        public long Number()
+        {
+            return accNo;
+        }
+        public decimal Balance()
+        {
+            return accBal;
+        }
+        public AccountType Type()
+        {
+            return accType;
+        }
+       
+
+        private long accNo;
+        private decimal accBal;
+        private AccountType accType;
+        
+    }
+}
---
+++ ITMO.CsharpProg2022.Lab6.2/BankAccount.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CsharpProg2022.Lab6._1
+{
+    class BankAccount
+    {
+        public void Populate(decimal balance)
+        {
+            accNo = NextNumber();
+            accBal = balance;
+            accType = AccountType.Checking;
+        }
+        public long Number()
+        {
+            return accNo;
+        }
+        public decimal Balance()
+        {
+            return accBal;
+        }
+        public AccountType Type()
+        {
+            return accType;
+        }
+        private static long NextNumber()
+        {
+            return nextAccNo++;
+        }
+        private long accNo;
+        private decimal accBal;
+        private AccountType accType;
+        private static long nextAccNo = 123;
+    }
+}
